50afdb700cd128213a0bd6a2e8207294a3fc31ab
Curi0usly <176598646+Curi0usly@users.noreply.github.com>
2025-12-17 02:43:05 +0300

    A little fix on main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 import sys
-if sys.argv<2:
+if len(sys.argv) < 2:
     print("The file format should be main.py <filename>")
 
 with open(sys.argv[1],"rb") as file:
     magic_string= file.read(16)
 
-with open("COnfig-file", "r") as config_file:
-    if config_file.get(magic_string):
-       print("your Extension is : ${config_file.get(magic_string)}")
-     
+
+
+
+
+
+
+
+# with open("COnfig-file", "r") as config_file:
+#     if config_file.get(magic_string):
+#        print("your Extension is : ${config_file.get(magic_string)}")
+#
