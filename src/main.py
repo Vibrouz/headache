b6d9a460e3e9a933f23955c1cf7574ba0d253d29
Barok-y <barokyeshibr@gmail.com>
2025-12-16 23:21:21 +0300

    Implement file reading and magic string extraction in main.py; add initial Extractor.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+import sys
+if sys.argv<2:
+    print("The file format should be main.py <filename>")
 
+with open(sys.argv[1],"rb") as file:
+    magic_string= file.read(16)
+
+with open("COnfig-file", "r") as config_file:
+    if config_file.get(magic_string):
+       print("your Extension is : ${config_file.get(magic_string)}")
+     
